3b3fe67136afb645bd90d5d71ff9bd0777abea1a
Alexander Mot <alexander@amot.dev>
2022-10-15 12:37:17 -0700

    Add basic login page

---
+++ lib/pages/login.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/auth_service.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Google Login"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        width: size.width,
+        height: size.height,
+        padding: EdgeInsets.only(
+            left: 20, right: 20, top: size.height * 0.2, bottom: size.height * 0.5),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text("Hello, \nGoogle sign in",
+                style: TextStyle(
+                    fontSize: 30
+                )),
+            GestureDetector(
+                onTap: () {
+                  AuthService().signInWithGoogle();
+                },
+                child: const Image(width: 100, image: AssetImage('assets/google.png'))),
+          ],
+        ),
+      ),
+    );
+  }
+}
