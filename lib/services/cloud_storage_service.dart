50010532b0b94464e19aba2a50af043b6f36fd6c
Alexander Mot <alexander@amot.dev>
2022-10-15 15:31:31 -0700

    Save

--- lib/services/cloud_storage_service.dart
+++ lib/services/cloud_storage_service.dart
@@ -1,3 +1,20 @@
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/services.dart';
+
+import '../classes/user.dart';
+
 class CloudStorageService {
-  
+  final storageRef = FirebaseStorage.instance.ref();
+
+  void syncUser(FlakeUser user) async {
+    if (user.instance.tenantId != null) {
+      final flakeUserDataRef = storageRef.child(user.instance.tenantId!);
+      try {
+        final Uint8List? data = await storageRef.getData(1024 * 1024);
+      } on FirebaseException catch (e) {
+        // Handle any errors.
+      }
+    }
+    
+  }
 }
